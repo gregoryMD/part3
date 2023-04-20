78ef93b38ec4229bcd62d6ed2efff0dddc4e1bb7
gregoryMD <gdiehl265@gmail.com>
2023-04-20 11:04:43 -0400

    mongoose implementation

---
+++ mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://gdiehl265:${password}@cluster0.rzxoox5.mongodb.net/phoneBookApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+});
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+} else if (process.argv.length > 3) {
+  person.save().then((result) => {
+    console.log("person saved!");
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+}

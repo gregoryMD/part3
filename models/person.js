82080c384d161ebff3ab5f0ef06feb40d7d18dba
gregoryMD <gdiehl265@gmail.com>
2023-04-27 13:51:52 -0400

    phone number validation

--- models/person.js
+++ models/person.js
@@ -21,7 +21,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number.`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
